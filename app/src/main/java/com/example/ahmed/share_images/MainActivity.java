7f98f8e856150466457cce3c281089c05353ade8
AhmedAtia39 <ahmedatia689@gmail.com>
2018-05-20 09:48:19 +0200

    done

--- app/src/main/java/com/example/ahmed/share_images/MainActivity.java
+++ app/src/main/java/com/example/ahmed/share_images/MainActivity.java
@@ -1,13 +1,82 @@
 package com.example.ahmed.share_images;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Uri uri;
+    ImageView imageView1, imageView2;
+    ArrayList<Uri> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+
+        arrayList = new ArrayList<>();
+
+        getPermission();
+    }
+
+    private void getPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 10);
+            }
+        }
+    }
+
+    public void share_img1(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+    }
+
+    public void share_img2(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 2);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        uri = data.getData();
+        if (resultCode == RESULT_OK && requestCode == 1) {
+            imageView1.setImageURI(uri);
+            arrayList.add(uri);
+        }
+
+        if (resultCode == RESULT_OK && requestCode == 2) {
+            imageView2.setImageURI(data.getData());
+            arrayList.add(uri);
+        }
+
+    }
+
+    public void btn_share(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+        intent.setType("image/*");
+        intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, arrayList);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(Intent.createChooser(intent, "مشاركه"));
+        }
     }
 }
